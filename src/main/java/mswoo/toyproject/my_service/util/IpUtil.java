5b1a8773f9e605fc77ee9645292bd348aa6e878c
MsWoo <minsoo010@naver.com>
2023-12-24 00:28:20 +0900

    - IP Util 추가

---
+++ src/main/java/mswoo/toyproject/my_service/util/IpUtil.java
@@ -0,0 +1,33 @@
+package mswoo.toyproject.my_service.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class IpUtil {
+
+    public static String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Daum-IP");
+
+        if (ip == null) {
+            ip = request.getHeader("X-FORWARDED-FOR");
+        }
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+}
