f82036382c892d16f005d688b23e319d6c51bbcf
MsWoo <minsoo010@naver.com>
2024-04-20 18:14:17 +0900

    - 공간, 예약 Entity 생성

---
+++ core_module/src/main/java/ms/toy/my_service/domain/entity/Space.java
@@ -0,0 +1,44 @@
+package ms.toy.my_service.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "space")
+@SuperBuilder
+@Data
+@DynamicInsert
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
+public class Space extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "spaceName", nullable = false, length = 10)
+    private String spaceName;
+
+    @Column(name = "spaceDescription", length = 30)
+    private String spaceDescription;
+
+    @Column(name = "capacity", nullable = false)
+    private Integer capacity;
+
+    @ColumnDefault("'N'")
+    @Column(name = "deleteYn", length = 1)
+    private String deleteYn;
+}
