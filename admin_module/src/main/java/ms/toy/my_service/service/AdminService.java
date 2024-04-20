a0e47f0d46c824e3a698dc0f682347a61662df3a
MsWoo <minsoo010@naver.com>
2024-04-20 18:00:47 +0900

    - MapStruct admin mapper 적용

--- admin_module/src/main/java/ms/toy/my_service/service/AdminService.java
+++ admin_module/src/main/java/ms/toy/my_service/service/AdminService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import ms.toy.my_service.domain.dto.*;
 import ms.toy.my_service.domain.entity.Admin;
 import ms.toy.my_service.enums.ErrorCode;
+import ms.toy.my_service.mapper.AdminMapper;
 import ms.toy.my_service.repository.AdminRepository;
 import ms.toy.my_service.repository.search.AdminSearchCondition;
 import org.springframework.data.domain.Page;
@@ -20,13 +21,14 @@ import java.util.stream.Collectors;
 public class AdminService {
     private final PasswordEncoder passwordEncoder;
     private final AdminRepository adminRepository;
+    private final AdminMapper adminMapper;
 
     public CommonPageDto searchAdmin(AdminSearchCondition adminSearchCondition) {
         Page<Admin> adminPage = adminRepository.searchAdmin(adminSearchCondition, adminSearchCondition.getPageable());
 
         return CommonPageDto.builder()
                 .list(adminPage.stream()
-                        .map(AdminDto::toDto)
+                        .map(adminMapper::toListDto)
                         .collect(Collectors.toList()))
                 .pageIndex(adminSearchCondition.getPageIndex())
                 .pageSize(adminSearchCondition.getPageSize())
@@ -35,11 +37,11 @@ public class AdminService {
                 .build();
     }
 
-    public AdminDto getAdminInfo(Long id) {
+    public AdminDetailDto getAdminInfo(Long id) {
         Admin admin = adminRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.OK, ErrorCode.EMPTY_DATA.name()));
 
-        return AdminDto.toDto(admin);
+        return adminMapper.toDetailDto(admin);
     }
 
     @Transactional(rollbackFor = Exception.class)
@@ -48,16 +50,10 @@ public class AdminService {
             throw new ResponseStatusException(HttpStatus.OK, ErrorCode.DUPLICATE_ID.name());
         }
 
-        // todo [gotoend] mapper로 전환 필요
-        // 필드가 추가될때마다 변경해줘야한다. mapper로 한방에 변환
-        Admin admin = Admin.builder()
-                .authorityId(2L)
-                .userId(adminJoinDto.getUserId())
-                .userName(adminJoinDto.getUserName())
-                .password(passwordEncoder.encode("1234"))
-                .phoneNumber(adminJoinDto.getPhoneNumber())
-                .createdBy(adminJoinDto.getUserId())
-                .build();
+        Admin admin = adminMapper.toEntity(adminJoinDto);
+        admin.setAuthorityId(2L);
+        admin.setPassword(passwordEncoder.encode("1234"));
+        admin.setCreatedBy(adminJoinDto.getUserId());
 
         Long id = adminRepository.save(admin).getId();
 
@@ -75,7 +71,6 @@ public class AdminService {
         Admin admin = adminRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.OK, ErrorCode.EMPTY_DATA.name()));
 
-        // todo [gotoend] mapper로 전환 필요
         admin.update(adminEditDto, userInfo.getUserId());
 
         return AdminDto.builder().id(id).build();
---
+++ core_module/src/main/java/ms/toy/my_service/config/MapstructConfig.java
@@ -0,0 +1,8 @@
+package ms.toy.my_service.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MapstructConfig {
+}
